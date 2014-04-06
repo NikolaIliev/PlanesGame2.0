a8d8fa910631c1823f7f043b9ebaa3cacace55bc
Nikola Iliev <nikola.iliev93@gmail.com>
2014-04-06 12:27:30 +0300

    Graphics + spawn animations

---
+++ js/Engine/Visual.js
@@ -0,0 +1,154 @@
+var Visual = {
+
+    backgroundOffset: 0,
+
+    //Makes the cursor invisible while game is active
+    adjustCSSofGameScreen: function (isStartMission) {
+        if (isStartMission) {
+            this.backgroundOffset = 0;
+            $("#gameScreen").css({
+                "cursor": "none",
+                "background-image": "url(../planesGame/images/backgrounds/river.jpg)"
+            });
+        }
+        else {
+            $("#gameScreen").css({
+                "cursor": "default",
+                "background-image": "none"
+            });
+        }
+    },
+
+    //Moves the background
+    iterateBackground: function () {
+        this.backgroundOffset++;
+        document.getElementById("gameScreen").style.backgroundPositionY = this.backgroundOffset + "px";
+    },
+
+    //Gives text for the primary mission Description
+    returnPrimaryDescription: function(mission){
+        if(mission instanceof SurvivalMission){
+            return "Survive in the battlefield for 45 seconds"
+        }
+        else if(mission instanceof DominationMission){
+            return "Don't let more than 7 enemies spawn for 30 seconds"
+        }
+        else if(mission instanceof GauntletMission){
+            return "Kill 75 enemies. Press E to summon additional ones."
+        }
+        else{
+            throw new Error("No such mission");
+        }
+    },
+
+
+    crossOutSecondaries: function(stat){
+        var conditions;
+        switch(interactionManager.getSecondaryMission()){
+            case "accuracy":
+                conditions=[25,35,50];
+                break;
+            case "remainingHealth":
+                conditions=[25,50,75];
+                break;
+            default:
+                break;
+        }
+        for(var i=0;i<conditions.length;i++){
+            if(conditions[i]>stat){
+                $("#listItem"+i).addClass("uncompletedSecondary");
+            }
+            else{
+                 $("#listItem"+i).removeClass("uncompletedSecondary");
+                }
+            }
+    },
+
+    setSecondaryDescriptions: function(){
+        var conditions;
+         switch(interactionManager.getSecondaryMission()){
+            case "accuracy":
+            conditions = [25,35,50];
+            for(var i=0;i<conditions.length;i++){
+                $("#listItem"+i).text("Keep your accuracy above "+conditions[i]+"%");
+            }
+            break;
+
+            case "remainingHealth":
+            conditions = [25,50,75];
+            for(var i=0;i<conditions.length;i++){
+                $("#listItem"+i).text("Keep your health above "+conditions[i]+"%");
+            }
+            break;
+        }
+    },
+
+    //Draws the user interface during missions
+    drawUI: function (mission) {
+
+        $("<ul/>")
+        .addClass("missionList")
+        .appendTo("#gameScreen");
+
+        for(var i=0;i<3;i++){
+            $("<li/>")
+            .attr("id","listItem"+i)
+            .addClass("secondaryListItem").
+            appendTo(".missionList");
+        }
+
+        this.setSecondaryDescriptions();
+
+        
+        $("<div/>")
+        .addClass("ui")
+        .appendTo("#gameScreen");
+
+        $("<div/>", {
+            id: "hpBar"
+        })
+        .appendTo(".ui");
+        var skillArray = interactionManager.getPlayerSkills();
+        for (var i = 0; i < 4; i++) {
+            $("<div/>", {
+                id: "skill" + i
+            })
+            .addClass("skills")
+            .appendTo(".ui");
+            //Places correct icons
+            if (skillArray[i] == undefined) { return;}
+            else {
+                $("#skill"+i).addClass(skillArray[i].icon);
+            }
+        }
+        //Places the primary mission
+          $("<span/>")
+        .addClass("mainMissionName")
+        .appendTo(".ui")
+        .text(this.returnPrimaryDescription(mission));
+
+    },
+
+    //Make a skill's icon grey
+    cooldownIcon : function(icon){
+        $("."+icon).css("background-image","url(images/UI/"+icon+"CD.png)");
+    },
+
+    activateIcon : function(icon){
+         $("."+icon).css("background-image","url(images/UI/"+icon+".png)");
+    },
+
+    //Creates the black hole animation
+    drawBlackHole : function(left,bottom){
+        $("<div/>")
+        .addClass("blackHole")
+        .css({
+            "left":left-200,
+            "bottom":bottom-200
+        })
+        .appendTo("#gameScreen");
+
+        setTimeout(function(){$('.blackHole').remove()},500);
+    }
+    
+};
--- js/GameObjects/Planes/AIPlane.js
+++ js/GameObjects/Planes/AIPlane.js
@@ -1,16 +1,51 @@
 ﻿AIPlane = Plane.extend({
     init: function (left, bottom, maxHealth, damage) {
         this._super(maxHealth, damage);
-        this.updateCoords(left, bottom);
-        this.move();
         this.hpBar = document.createElement('div');
         $(this.hpBar).appendTo(this.div);
+        this.updateCoords(left, bottom);
+        this.move();
     },
 
     hpBar: null,
 
     updateHpBar: function () {
         this.hpBar.style.width = parseInt(this.currentHealth / this.maxHealth * 100) + '%';
+    },
+
+    animateSpawn: function () {
+        var rand = parseInt(Math.random() * 2),
+            currentLeftCoord = this.leftCoord;
+
+        switch (rand) {
+            case 0:
+                this.leftCoord = 0; //plane comes from the left side
+                break;
+            case 1:
+                this.leftCoord = 960; //plane comes from the right side
+                break;
+            default:
+                throw new Error('Error generating a random number [0, 2]');
+        }
+
+        this.move();
+        $(this.div).css('opacity', '0');
+        $(this.div).animate({
+            'left': currentLeftCoord,
+            'opacity': '1'
+        }, {
+            step: function (now, fx) {
+                if (fx.prop == 'opacity') {
+                    console.log(now);
+                    $(this).css('-webkit-transform', 'scale(' + now + ', ' + now + ')');
+                }
+            },
+
+            duration: 1500
+        });
+
+        this.leftCoord = currentLeftCoord;
+        this.move();
     },
 
     die: function () {
